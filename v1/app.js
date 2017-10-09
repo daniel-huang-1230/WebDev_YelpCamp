fd87e25a37439b58a40795be9b699db42d5882a6
Daniel Huang <dah0417912@gmail.com>
2017-10-09 05:33:38 +0000

    layout partially done

--- v1/app.js
+++ v1/app.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set("view engine", "ejs");
 
 app.get("/", function(req,res){
@@ -12,14 +16,31 @@ var campgrounds = [
         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
 ];
 
 app.get("/campgrounds", function(req, res){
     res.render("campgrounds", {campgrounds:campgrounds});
 }); 
 
-
+app.get("/campgrounds/new", function(req,res){
+    res.render("new");
+});
+app.post("/campgrounds", function(req, res){
+    //get data from the form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name:name, image:image};
+    campgrounds.push(newCampground);
+    //yes we do have two /campgrounds routes, but for redirect,
+    //the default route is to the GET request
+    res.redirect("/campgrounds");   
+});
 app.listen(process.env.PORT, process.env.IP, function(){
 
     console.log("The Yelp Camp server has started！");
