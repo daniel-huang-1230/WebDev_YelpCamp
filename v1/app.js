0f8846be88ad09f3a98410a6dc7212ffdaffbcdb
Daniel Huang <dah0417912@gmail.com>
2017-10-11 20:32:48 +0000

    dababase setup partially

--- v1/app.js
+++ v1/app.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express"),
+    app         =  express(),
+    bodyParser  = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -22,10 +22,10 @@ var campgrounds = [
         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        
-         
-       
+   
 ];
+
+
 
 app.get("/campgrounds", function(req, res){
     res.render("campgrounds", {campgrounds:campgrounds});
--- v2/app.js
+++ v2/app.js
@@ -1,7 +1,9 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express"),
+    app         =  express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine", "ejs");
@@ -12,23 +14,34 @@ app.get("/", function(req,res){
 });
 
 
-var campgrounds = [
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        
-         
-       
-];
 
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create( {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},function(err,campground){
+//   if(err){
+//       console.log(err);
+//   }else{
+//       console.log("Newly created campground: ");
+//       console.log(campground);
+//   }
+   
+// });
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds:campgrounds});
+    //GET all campgrounds from the database
+    Campground.find({},function(err,allCampgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds", {campgrounds:allCampgrounds});
+        }
+    });
+  
 }); 
 
 app.get("/campgrounds/new", function(req,res){
@@ -39,10 +52,20 @@ app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name:name, image:image};
-    campgrounds.push(newCampground);
-    //yes we do have two /campgrounds routes, but for redirect,
-    //the default route is to the GET request
-    res.redirect("/campgrounds");   
+  
+    //add the newly created campground to the database
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+            //yes we do have two campgrounds routes, but for redirect,
+            //the default route is to the GET request
+            res.redirect("/campgrounds");   
+        }
+    });
+  
+  
+    
 });
 app.listen(process.env.PORT, process.env.IP, function(){
 
